ea09b1bebf8d6f44e3a02bac75fee84c13e1bf89
Abhishek Tenneti <abhishek.tenneti@providence.org>
2021-03-09 20:34:51 +0530

    'BurgerHandlersCommits'

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,15 @@
 import React,{Component} from 'react';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Auxi';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const  INGREDIENT_PRICES = {
+    salad : 1,
+    cheese : 1,
+    meat : 1,
+    bacon : 1
+} 
+
 
 class BurgerBuilder extends Component{
     // constructor(){
@@ -11,19 +20,41 @@ class BurgerBuilder extends Component{
     // }
     state={
         ingredients : {
-            salad : 1,
-            bacon : 1,
-            cheese : 2,
-            meat : 2
-        }
+            salad : 0,
+            bacon : 0,
+            cheese :0,
+            meat : 0
+        },
+        totalPrice : 10
     }
+
+    addIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount+1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients.type = updatedCount;
+        const newPrice = this.state.totalPrice+INGREDIENT_PRICES[type];
+        this.setState({totalPrice:newPrice,ingredients:updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) =>{
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount-1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients.type = updatedCount;
+        const newPrice = this.state.totalPrice-INGREDIENT_PRICES[type];
+        this.setState({totalPrice:newPrice,ingredients:updatedIngredients})
+    }
+
     render (){
         return(
             <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <div>
-                    Build Controls
-                </div>
+                <Burger ingredients={this.state.ingredients} />                
+                <BuildControls/>               
             </Aux>
         );
     }
